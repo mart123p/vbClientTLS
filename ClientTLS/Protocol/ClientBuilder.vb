d63c1fd9842fcb27077a4707344f36c701437ba8
Yan-Eric Cossette <yan.eric.cossette.01@edu.cegeptr.qc.ca>
2016-11-29 17:10:13 -0500

    added etudiant

---
+++ ClientTLS/Protocol/ClientBuilder.vb
@@ -0,0 +1,31 @@
+﻿Imports Newtonsoft.Json.Linq
+Public Class ClientBuilder
+    Dim _auth As String
+    Dim _request As String
+
+    Public Function createAccount(ByVal etudiant As Etudiants) As String
+        Dim jo As New JObject
+        jo.Add("firstName", etudiant.firstName)
+        Return ""
+    End Function
+    Public Function studyField() As String
+
+        Return ""
+    End Function
+    Public Function connection() As String
+
+        Return ""
+    End Function
+    Public Function disconnect() As String
+
+        Return ""
+    End Function
+    Public Function studentDirectory() As String
+
+        Return ""
+    End Function
+    Public Function profileManager() As String
+
+        Return ""
+    End Function
+End Class
---
+++ ClientTLS/Protocol/ProtocolStatus.vb
@@ -0,0 +1,7 @@
+﻿Public Enum ProtocolStatus
+    OK = 200
+    UNAUTHORIZED = 401
+    NOT_FOUND = 404
+    BAD_REQUEST = 400
+    CONFLICT = 409
+End Enum

14c14ec75bd5fcc23cc8b9805e4c869180cc9c19
Martin Pouliot <martin.pouliot.01@edu.cegeptr.qc.ca>
2016-11-29 16:14:24 -0500

    Initial commit

---
+++ ClientTLS/Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+    Dim crypto As CryptoTLS
+    Dim socketTLS As SocketTLS
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        crypto = New CryptoTLS(False)
+        socketTLS = New SocketTLS(SocketTLSType.Client, crypto, TextBox1.Text, 5000, AddressOf receiver, AddressOf onConnect)
+    End Sub
+
+    Private Sub receiver(ByVal request As EtudiantsRequest)
+        Console.WriteLine(request.getRequest)
+    End Sub
+
+    Private Sub onConnect(ByVal request As EtudiantsRequest)
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        socketTLS.Send("TEstTEstTEsstTEstTEstTEstTEstTEstTEstTEstTEst")
+    End Sub
+End Class
---
+++ ClientTLS/TLS/EtudiantsProtocol/EtudiantsProtocol.vb
@@ -0,0 +1,15 @@
+﻿Public Class EtudiantsProtocol
+    Dim request As String
+
+    Sub New(ByVal str As String)
+        request = str
+    End Sub
+
+    Sub New()
+
+    End Sub
+
+
+
+
+End Class

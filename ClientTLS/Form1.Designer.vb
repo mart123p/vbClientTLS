8010338d5f6c442db00482ab03a651362b5b8573
Martin Pouliot <martinp507@gmail.com>
2016-12-04 02:17:55 -0500

    Changed the interface and socketTLS

--- ClientTLS/Form1.Designer.vb
+++ ClientTLS/Form1.Designer.vb
@@ -26,52 +26,83 @@ Partial Class Form1
         Me.Button2 = New System.Windows.Forms.Button()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(197, 97)
+        Me.Button1.Location = New System.Drawing.Point(263, 12)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.Size = New System.Drawing.Size(100, 28)
         Me.Button1.TabIndex = 0
         Me.Button1.Text = "Connexion"
         Me.Button1.UseVisualStyleBackColor = True
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(39, 97)
+        Me.Button2.Location = New System.Drawing.Point(263, 126)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.Size = New System.Drawing.Size(100, 28)
         Me.Button2.TabIndex = 1
-        Me.Button2.Text = "Button2"
+        Me.Button2.Text = "Envoyer"
         Me.Button2.UseVisualStyleBackColor = True
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(39, 12)
+        Me.TextBox1.Location = New System.Drawing.Point(52, 15)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.Size = New System.Drawing.Size(132, 22)
         Me.TextBox1.TabIndex = 2
         Me.TextBox1.Text = "127.0.0.1"
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(12, 15)
+        Me.Label1.Location = New System.Drawing.Point(16, 18)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(17, 13)
+        Me.Label1.Size = New System.Drawing.Size(20, 17)
         Me.Label1.TabIndex = 3
         Me.Label1.Text = "IP"
         '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(20, 48)
+        Me.TextBox2.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(341, 69)
+        Me.TextBox2.TabIndex = 4
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox3.Location = New System.Drawing.Point(20, 175)
+        Me.TextBox3.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.TextBox3.Multiline = True
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.ReadOnly = True
+        Me.TextBox3.Size = New System.Drawing.Size(341, 73)
+        Me.TextBox3.TabIndex = 5
+        '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.ClientSize = New System.Drawing.Size(379, 282)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
@@ -83,4 +114,6 @@ Partial Class Form1
     Friend WithEvents Button2 As Button
     Friend WithEvents TextBox1 As TextBox
     Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
 End Class
--- ClientTLS/Form1.vb
+++ ClientTLS/Form1.vb
@@ -1,21 +1,42 @@
-﻿Public Class Form1
+﻿Imports System.Net.Sockets
+
+Public Class Form1
     Dim crypto As CryptoTLS
     Dim socketTLS As SocketTLS
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         crypto = New CryptoTLS(False)
-        socketTLS = New SocketTLS(SocketTLSType.Client, crypto, TextBox1.Text, 5000, AddressOf receiver, AddressOf onConnect)
+        Try
+            socketTLS = New SocketTLS(SocketTLSType.Client, crypto, TextBox1.Text, 5000, AddressOf receiver, AddressOf onConnect, AddressOf onDisconnect)
+        Catch ex As KeyPairChanged
+
+            If MessageBox.Show("La clé publique a changé. Ceci peut poser un risque de sécurité. Voulez vous continuer malgré les circonstances?", "Avertissement", MessageBoxButtons.YesNo) = DialogResult.Yes Then
+                Try
+                    socketTLS = New SocketTLS(SocketTLSType.Client, crypto, TextBox1.Text, 5000, AddressOf receiver, AddressOf onConnect, AddressOf onDisconnect, True)
+                Catch ex2 As SocketException
+                    MessageBox.Show("Le serveur n'est pas trouvable", "Erreur")
+                End Try
+            End If
+
+        Catch ex As SocketException
+            MessageBox.Show("Le serveur n'est pas trouvable", "Erreur")
+        End Try
+
     End Sub
 
     Private Sub receiver(ByVal request As EtudiantsRequest)
-        Console.WriteLine(request.getRequest)
+        TextBox3.Text = request.getRequest
     End Sub
 
     Private Sub onConnect(ByVal request As EtudiantsRequest)
 
     End Sub
 
+    Private Sub onDisconnect(ByVal endpoint As String)
+
+    End Sub
+
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        socketTLS.Send("TEstTEstTEsstTEstTEstTEstTEstTEstTEstTEstTEst")
+        socketTLS.Send(TextBox2.Text)
     End Sub
 
 End Class
